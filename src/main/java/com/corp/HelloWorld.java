dd262db32e2fd17edf46f82fecc0c51a18407f70
Andrey Fedyakov <ya.andreyfedyakov@yandex.ru>
2022-11-22 01:26:47 +0300

    Jar task. Part 1

--- src/main/java/com/corp/HelloWorld.java
+++ src/main/java/com/corp/HelloWorld.java
@@ -1,9 +1,11 @@
 package com.corp;
 
+import org.springframework.util.StringUtils;
+
 public class HelloWorld {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        System.out.println(StringUtils.capitalize("hello world"));
     }
 }
